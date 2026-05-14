52c76f512120a9ce70891c1abe399e571153de79
Palthi Spandhana <palthispandhana812@gmail.com>
2026-05-14 13:54:40 +0530

    Added React Three Fiber interactive animated 3D scene

--- src/components/ThreeScene.jsx
+++ src/components/ThreeScene.jsx
@@ -1,9 +1,19 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Float } from "@react-three/drei";
+import { useRef } from "react";
 
 function Cube() {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+
   return (
-    <mesh rotation={[0.5, 0.5, 0]}>
+    <mesh ref={meshRef}>
       <boxGeometry args={[2, 2, 2]} />
 
       <meshStandardMaterial
@@ -18,25 +28,33 @@ function Cube() {
 const ThreeScene = () => {
   return (
     <Canvas
+      camera={{ position: [0, 0, 5] }}
       style={{
         width: "100vw",
         height: "100vh",
         background: "#111111",
       }}
     >
-      {/* Lights */}
+      {/* Ambient Light */}
       <ambientLight intensity={0.5} />
 
+      {/* Directional Light */}
       <directionalLight
-        position={[2, 2, 5]}
-        intensity={1}
+        position={[5, 5, 5]}
+        intensity={2}
       />
 
-      {/* Cube */}
-      <Cube />
+      {/* Floating Cube */}
+      <Float
+        speed={2}
+        rotationIntensity={1}
+        floatIntensity={2}
+      >
+        <Cube />
+      </Float>
 
-      {/* Controls */}
-      <OrbitControls />
+      {/* Camera Controls */}
+      <OrbitControls enableZoom={true} />
     </Canvas>
   );
 };
